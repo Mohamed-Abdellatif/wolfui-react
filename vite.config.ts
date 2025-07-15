a96f2cee052fe69e9e23cba8c3bb625d07825041
Mohamed-Abdellatif <mabdellatif411@gmail.com>
2025-07-15 17:12:44 +0300

    implement storybook for visualization

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,13 @@
+/// <reference types="vitest/config" />
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 import { fileURLToPath } from "url";
+import { storybookTest } from '@storybook/addon-vitest/vitest-plugin';
+const dirname = typeof __dirname !== 'undefined' ? __dirname : path.dirname(fileURLToPath(import.meta.url));
 
+// More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
-
 export default defineConfig({
   plugins: [react()],
   build: {
@@ -12,16 +15,39 @@ export default defineConfig({
       entry: path.resolve(__dirname, "src/index.ts"),
       name: "wolfui-react",
       formats: ["es", "umd"],
-      fileName: (format) => `wolfui-react.${format}.js`,
+      fileName: format => `wolfui-react.${format}.js`
     },
     rollupOptions: {
       external: ["react", "react-dom"],
       output: {
         globals: {
           react: "React",
-          "react-dom": "ReactDOM",
-        },
-      },
-    },
+          "react-dom": "ReactDOM"
+        }
+      }
+    }
   },
+  test: {
+    projects: [{
+      extends: true,
+      plugins: [
+      // The plugin will run tests for the stories defined in your Storybook config
+      // See options at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon#storybooktest
+      storybookTest({
+        configDir: path.join(dirname, '.storybook')
+      })],
+      test: {
+        name: 'storybook',
+        browser: {
+          enabled: true,
+          headless: true,
+          provider: 'playwright',
+          instances: [{
+            browser: 'chromium'
+          }]
+        },
+        setupFiles: ['.storybook/vitest.setup.ts']
+      }
+    }]
+  }
 });
